073926698537ef39a067841486ca8e8c1a0d97e5
Logan Wood <lwood3499@gmail.com>
2019-01-16 10:42:10 -0600

    testing codesandbox.io
    Testing codesandbox.io commit process.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import NavItems from "./components/Navigation/NavItems/NavItems";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>7th Grade Science</h1>
-                <Button variant="raised" color="primary">
-                    First Button
-                </Button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>7th Grade Science</h1>
+        <NavItems />
+        <Button variant="raised" color="primary">
+          First Button
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navigation/NavItems/NavItems.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./NavItems.module.css";
+
+class NavItems extends Component {
+  render() {
+    return <h1 className={classes.NavItems}>Nav Item 1</h1>;
+  }
+}
+
+export default NavItems;
