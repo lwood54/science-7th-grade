1d9c46f426733aa6c4a75292a0d472fba4c06dfd
Logan Wood <lwood3499@gmail.com>
2019-01-15 22:20:44 -0600

    added material ui library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <h1>7th Grade Science</h1>
+                <Button variant="raised" color="primary">
+                    First Button
+                </Button>
             </div>
         );
     }
