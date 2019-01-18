4a84a5b7742054b5d909ebe23685a8fbd60d8fcd
Logan Wood <lwood3499@gmail.com>
2019-01-17 23:00:19 -0600

    some menu color changes

--- src/components/UI/MainMenu/MainMenu.js
+++ src/components/UI/MainMenu/MainMenu.js
@@ -1,17 +1,22 @@
-import React from "react";
-import Drawer from "@material-ui/core/Drawer";
-import MenuItems from "./MenuItems/MenuItems";
+import React from 'react';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItems from './MenuItems/MenuItems';
+import cls from './MainMenu.module.css';
 
 const MainMenu = props => {
-  return (
-    <div>
-      <Drawer open={props.openedMenu} onClose={props.toggleMenu}>
-        <div onClick={props.toggleMenu} onKeyDown={props.toggleMenu}>
-          <MenuItems />
+    return (
+        <div>
+            <Drawer open={props.openedMenu} onClose={props.toggleMenu}>
+                <div
+                    onClick={props.toggleMenu}
+                    onKeyDown={props.toggleMenu}
+                    className={cls.Drawer}
+                >
+                    <MenuItems />
+                </div>
+            </Drawer>
         </div>
-      </Drawer>
-    </div>
-  );
+    );
 };
 
 export default MainMenu;
--- src/components/UI/MainMenu/MenuItems/MenuItems.js
+++ src/components/UI/MainMenu/MenuItems/MenuItems.js
@@ -1,22 +1,25 @@
-import React from "react";
-import List from "@material-ui/core/List";
-import Button from "@material-ui/core/Button";
-import PageList from "../../../ContentPages/PageList/PageList";
-import cls from "./MenuItems.module.css";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import PageList from '../../../ContentPages/PageList/PageList';
+import cls from './MenuItems.module.css';
 
 // returns array from PageList
 const pageList = PageList();
 
 const menuItems = () => {
-  const items = pageList.map(unit => {
-    return (
-      <Button className={cls.Button} onClick={() => alert(unit.pageAddress)}>
-        {unit.unitName}
-      </Button>
-    );
-  });
+    const items = pageList.map(unit => {
+        return (
+            <Button
+                key={unit.unitName}
+                className={cls.Button}
+                onClick={() => alert(unit.pageAddress)}
+            >
+                {unit.unitName}
+            </Button>
+        );
+    });
 
-  return <div>{items}</div>;
+    return <div>{items}</div>;
 };
 
 export default menuItems;
