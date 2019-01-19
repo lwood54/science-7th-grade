486f5c8182473f5f5af47ebd7558f4a6fb4d5d64
Logan Wood <lwood3499@gmail.com>
2019-01-19 00:08:43 -0600

    centering page component titles

--- src/components/ContentPages/HomePage/HomePage.js
+++ src/components/ContentPages/HomePage/HomePage.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React from 'react';
+
+import cls from '../ContentPages.module.css';
 
 const homePage = () => {
-  return <h1>Home Page</h1>;
+    return <h1 className={cls.PageTitle}>Home Page</h1>;
 };
 
 export default homePage;
--- src/components/ContentPages/Unit1_1/Unit1_1.js
+++ src/components/ContentPages/Unit1_1/Unit1_1.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import cls from '../ContentPages.module.css';
 
 const unit1_1 = () => {
-  return <h1>Unit 1.1 Page</h1>;
+    return <h1 className={cls.PageTitle}>Unit 1.1 Page</h1>;
 };
 
 export default unit1_1;
