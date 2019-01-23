58f29b2d1f5adc674ea848d78513414d093cbbc1
Logan Wood <lwood3499@gmail.com>
2019-01-23 15:21:07 -0600

    initial formatting in unit
    Adding material-ui paper and connecting TEKS objects

--- src/components/ContentPages/ContentPageTemplate/ContentPageTemplate.js
+++ src/components/ContentPages/ContentPageTemplate/ContentPageTemplate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
 
 import NavigationBar from "../../Navigation/NavigationBar/NavigationBar";
 
@@ -6,6 +7,20 @@ const ContentPageTemplate = props => {
   // I want to put this Component into each page and pass the title to the top navbar when it loads
   // If I put the nav bar in here and make it similar to the home page and simply put this at the top of each page and
   // add the props.title based on the page that I'm on.
+
+  const listOfCards = props.TEKSArray.map(teksObj => {
+    const { classes } = props;
+    console.log(classes);
+    // LOOK IN TO HOW TO MORE EASILY MANIPULATE build in Material-UI classes
+    return (
+      <div>
+        <Paper>
+          <h2>{teksObj.teksNum}</h2>
+          <h3>{teksObj.teksLabel}</h3>
+        </Paper>
+      </div>
+    );
+  });
   return (
     <div>
       <NavigationBar
@@ -14,6 +29,7 @@ const ContentPageTemplate = props => {
         quizletLink={props.quizletLink}
         homeLink={props.homeLink}
       />
+      {listOfCards}
     </div>
   );
 };
--- src/components/ContentPages/Unit1_1/Unit1_1.js
+++ src/components/ContentPages/Unit1_1/Unit1_1.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import Unit1_1Obj from "./Unit1_1Obj";
 import ContentPageTemplate from "../ContentPageTemplate/ContentPageTemplate";
+import TEKS from "../TEKS";
+
+// Custom identify specific TEKS that will be connected to this Unit Object
+// SUBJECT TO CHANGE
+// this variable may need to be changed in the future if they TEKS get rearranged by unit
+console.log("TEKS: ", TEKS);
+const TEKSArray = [TEKS[18], TEKS[20]];
 
 const unit1_1 = props => {
   // search for keys this way instead of hard coding array index because I may change that in the future.
@@ -27,6 +34,7 @@ const unit1_1 = props => {
         gameLink={gameLink}
         quizletLink={quizletLink}
         homeLink={homeLink}
+        TEKSArray={TEKSArray}
       />
     </React.Fragment>
   );
