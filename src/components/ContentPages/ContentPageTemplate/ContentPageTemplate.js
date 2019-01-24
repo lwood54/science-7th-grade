c42a4097a246e70727a948e9cf37be9a3af297e6
Logan Wood <lwood3499@gmail.com>
2019-01-24 16:37:20 -0600

    Adjusted ContentPageTemplate
    customizing unit pages vs using page template

--- src/components/ContentPages/ContentPageTemplate/ContentPageTemplate.js
+++ src/components/ContentPages/ContentPageTemplate/ContentPageTemplate.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
 
-import NavigationBar from '../../Navigation/NavigationBar/NavigationBar';
+import NavigationBar from "../../Navigation/NavigationBar/NavigationBar";
 
 const ContentPageTemplate = props => {
-    const { classes } = props;
-    console.log('CLASSES: ', classes);
-    //  TODO: LOOK IN TO HOW TO MORE EASILY MANIPULATE build in Material-UI classes
+  //  TODO: LOOK IN TO HOW TO MORE EASILY MANIPULATE build in Material-UI classes
 
-    const listOfCards = props.TEKSArray.map(teksObj => {
-        console.log(teksObj);
-        return (
-            <div key={teksObj.teksNum}>
-                <Paper>
-                    <h2>{teksObj.teksNum}</h2>
-                    <h3>{teksObj.teksLabel}</h3>
-                </Paper>
-            </div>
-        );
-    });
+  const listOfCards = props.TEKSArray.map(teksObj => {
+    console.log("INSIDE ContentPageTemplate: ", teksObj);
     return (
-        <div>
-            <NavigationBar
-                title={props.title}
-                gameLink={props.gameLink}
-                quizletLink={props.quizletLink}
-                homeLink={props.homeLink}
-            />
-            {listOfCards}
-        </div>
+      <div key={teksObj.teksNum}>
+        <Paper>
+          <h2>{teksObj.teksNum}</h2>
+          <h3>{teksObj.teksLabel}</h3>
+          {props.children}
+        </Paper>
+      </div>
     );
+  });
+  return (
+    <div>
+      <NavigationBar
+        title={props.title}
+        gameLink={props.gameLink}
+        quizletLink={props.quizletLink}
+        homeLink={props.homeLink}
+      />
+      {listOfCards}
+    </div>
+  );
 };
 
 export default ContentPageTemplate;
