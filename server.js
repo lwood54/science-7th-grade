6bf25b1c6ff06e66481c0efa1822f11a31bd81b1
Logan Wood <lwood3499@gmail.com>
2019-02-10 11:16:07 -0600

    Unit 5.1 game complete

--- server.js
+++ server.js
@@ -13,3 +13,24 @@ app.get('/*', function(req, res) {
 app.listen(port, () => {
         console.log('Server is up on ', port, '!');
 });
+
+// Things to do before deployment to heroku:
+//      https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08
+// 1. inside project directory, create server.js
+// 2. run express server, make sure path is linked to run index.html
+// 3. update package.json scripts to have
+//      a. start: "node server.js",
+//      b. dev: "react-scripts start"
+// 4. npm run build (in order to setup the build of production files)
+// 5. update .gitignore
+//      a. remove /build
+//      b. add:
+//              src/*
+//              public/*
+//              build/static/css/*.map
+//              build/static/js/*.map
+// 6. heroku create app-name-here
+//      (this will automatically set the heroku git repo as a remote repo)
+//      https://devcenter.heroku.com/articles/git
+// 7. add and commit git
+// 8. finally git push heroku master
--- src/components/ContentPages/UnitContent/Unit5_1/Unit5_1Obj.js
+++ src/components/ContentPages/UnitContent/Unit5_1/Unit5_1Obj.js
@@ -60,21 +60,21 @@ const unit5_1Obj = {
                 col3: {
                         heading: 'Biome',
                         hint: 'Tundra, Taiga, Temperate Forest, Tropical Rainforest, etc',
-                        image: '',
+                        image: require('./images/biome.jpg'),
                         definition:
                                 'A large naturally occurring community of plants and animals occupying a major habitat'
                 },
                 col4: {
                         heading: 'Ecoregion',
                         hint: "Pineywoods, Gulf Prairie, Edward's Plateau, etc",
-                        image: '',
+                        image: require('./images/ecoregions.png'),
                         definition:
                                 'A major ecosystem defined by distinctive geography and receiving uniform solar radiation and moisture'
                 },
                 col5: {
                         heading: 'Sustainability',
                         hint: 'Save the trees, keep our planet balanced and healthy',
-                        image: '',
+                        image: require('./images/sustainability.jpg'),
                         definition:
                                 'Supporting long-term ecological balance. Maintaining a healthy ecosystem.'
                 }
@@ -83,14 +83,14 @@ const unit5_1Obj = {
                 col1: {
                         heading: 'Tundra',
                         hint: 'Extremely cold, very low biodiversity',
-                        image: '',
+                        image: require('./images/gifs/tundra.gif'),
                         definition:
                                 'A vast, flat, treeless Arctic region of Europe, Asia, and North America in which the subsoil is permanently frozen'
                 },
                 col2: {
                         heading: 'Taiga/Boreal',
                         hint: 'A forest that looks like a bunch of Christmas trees',
-                        image: '',
+                        image: require('./images/gifs/taiga.gif'),
                         definition:
                                 'The sometimes swampy coniferous forest of high northern latitudes. Largest land-based biome on Earth.'
                 },
@@ -98,21 +98,21 @@ const unit5_1Obj = {
                         heading: 'Temperate Forest',
                         hint:
                                 'United States of America has many of these types of forests. Can include deciduous, coniferous, or broadleafed trees',
-                        image: '',
+                        image: require('./images/gifs/temperate_forest.gif'),
                         definition:
                                 'A forest found between the tropics and polar regions. This forest has the most significant seasonal changes.'
                 },
                 col4: {
                         heading: 'Tropical Rainforest',
                         hint: 'Amazon Jungle is an example',
-                        image: '',
+                        image: require('./images/gifs/rainforest.gif'),
                         definition:
                                 'A forest found in the tropics which receives an anual rainfall of at least 100 inches'
                 },
                 col5: {
                         heading: 'Desert',
                         hint: 'can be hot or cold, but has little rain',
-                        image: '',
+                        image: require('./images/gifs/desert.gif'),
                         definition:
                                 'A region so arid (lacking precipitation) that it supports very little vegetation or none at all'
                 }
