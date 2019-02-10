578718517c01cccbaab753aba33072d8585a9411
Logan Wood <lwood3499@gmail.com>
2019-02-10 17:51:51 -0600

    completed structuring and styling, ready for content

--- src/components/Templates/NotesTemplate/NotesTemplate.js
+++ src/components/Templates/NotesTemplate/NotesTemplate.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { Link } from 'react-router-dom';
 
-import Button from "@material-ui/core/Button";
+import Button from '@material-ui/core/Button';
 
-import cls from "./TEKSNotes.module.css";
+import cls from './NotesTemplate.module.css';
 
 //////////////////// REQUIRED PROPS  /////////////////////////////////
 //	teksNum, teksContent, teksQuiz, title //
@@ -26,35 +26,50 @@ import cls from "./TEKSNotes.module.css";
 	Option 2:
 		<div className={cls.NotesContainer}>
 			<div className={cls.Notes}>
+				<p>CONTENT</p>
 			</div>
 			<div className={cls.wideParagraph}>
+				<p>CONTENT</p>
+			</div>
+		</div>
+
+	Option 3:
+		<div className={cls.NotesContainer}>
+			<div className={cls.FullImageContainer}>
+				<img className={cls.Image} />
+			</div>
+		</div>
+		<div className={cls.NotesContainer}>
+			<div className={cls.wideParagraph}>
+				<p>CONTENT</p>
 			</div>
 		</div>
 */
 /////////////////////////////////////////////////////////////
 
 const notesTemplate = props => {
-	return (
-		<Paper className={cls.ContentContainer}>
-			<h1 className={cls.TEKSTitle}>{props.teksNum}</h1>
-			<Link to={props.teksQuiz} className={cls.TEKS}>
-				<p style={{ marginTop: "0" }}>{props.teksContent}</p>
-			</Link>
-			<h2 className={cls.Heading}>{props.title}</h2>
-			{props.children}
-			<div className={cls.QuizButtonContainer}>
-				<Link className={cls.Link} to={props.teksQuiz}>
-					<Button
-						color="secondary"
-						variant="contained"
-						className={cls.QuizButton}
-					>
-						Review Quiz
-					</Button>
-				</Link>
-			</div>
-		</Paper>
-	);
+        return (
+                <Paper className={cls.ContentContainer}>
+                        <h1 className={cls.Heading}>{props.title}</h1>
+                        <h3 className={cls.TEKSTitle}>Learning Objective: {props.teksNum}</h3>
+                        <Link to={props.teksQuiz} className={cls.TEKS}>
+                                <p style={{ marginTop: '0' }}>{props.teksContent}</p>
+                        </Link>
+
+                        {props.children}
+                        <div className={cls.QuizButtonContainer}>
+                                <Link className={cls.Link} to={props.teksQuiz}>
+                                        <Button
+                                                color="secondary"
+                                                variant="contained"
+                                                className={cls.QuizButton}
+                                        >
+                                                Review Quiz
+                                        </Button>
+                                </Link>
+                        </div>
+                </Paper>
+        );
 };
 
 export default notesTemplate;
