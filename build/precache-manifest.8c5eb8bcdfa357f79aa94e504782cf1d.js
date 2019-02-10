2e259f178e4cfd628440d23cdcc6f460d9deef63
Logan Wood <lwood3499@gmail.com>
2019-02-10 00:42:11 -0600

    setting up build, adding express server

---
+++ build/precache-manifest.8c5eb8bcdfa357f79aa94e504782cf1d.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aedf886d5e6d920c076",
+    "url": "/static/css/main.b35225a9.chunk.css"
+  },
+  {
+    "revision": "0aedf886d5e6d920c076",
+    "url": "/static/js/main.0aedf886.chunk.js"
+  },
+  {
+    "revision": "2ea8234178c8ed253087",
+    "url": "/static/js/1.2ea82341.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a0ba657dad6d8aba933bb359de33112",
+    "url": "/static/media/hurricane.4a0ba657.jpg"
+  },
+  {
+    "revision": "a3fd1019682a454d1b6ab6ea88943e8f",
+    "url": "/static/media/volcano.a3fd1019.jpg"
+  },
+  {
+    "revision": "05f46def418affe7ec25b2a14ac72545",
+    "url": "/static/media/volcanic_ash.05f46def.jpg"
+  },
+  {
+    "revision": "949200708c0a31b90a8060b903e37c02",
+    "url": "/static/media/tornado_with_farm.94920070.jpg"
+  },
+  {
+    "revision": "17840a45e132963942c1f9a0dd8752e4",
+    "url": "/static/media/wildfire.17840a45.jpg"
+  },
+  {
+    "revision": "4b3f9d345ce6ca54aa43e64673ce67f2",
+    "url": "/static/media/microhabitat.4b3f9d34.gif"
+  },
+  {
+    "revision": "ebac94f123435824d80d86093d05ee1c",
+    "url": "/static/media/levels_of_organization.ebac94f1.jpg"
+  },
+  {
+    "revision": "3d261cab94f7572e8914aa081c8fc1b1",
+    "url": "/static/media/keep_going.3d261cab.gif"
+  },
+  {
+    "revision": "5c359f7aaac1fc40177fadea9a6965e9",
+    "url": "/static/media/victory_royale.5c359f7a.gif"
+  },
+  {
+    "revision": "8633a3e6a7f9dbd8bd4771823a0dca60",
+    "url": "/static/media/dichot_leaves.8633a3e6.jpg"
+  },
+  {
+    "revision": "0b6694b944f8ce4eac959a1c03f14257",
+    "url": "/static/media/overgrown.0b6694b9.jpg"
+  },
+  {
+    "revision": "39664e0d5e55fac3f2ab27d496952093",
+    "url": "/static/media/primary_succession.39664e0d.jpg"
+  },
+  {
+    "revision": "d0f5ab9df0df4f42538f0b96faea5104",
+    "url": "/static/media/lichens_on_rock.d0f5ab9d.jpg"
+  },
+  {
+    "revision": "6feb5a4a117448e22e21e6086d3d35d2",
+    "url": "/static/media/lichens2.6feb5a4a.jpg"
+  },
+  {
+    "revision": "76b84cb42de38892107a6764d38b7a47",
+    "url": "/static/media/lichens_symbiosis.76b84cb4.gif"
+  },
+  {
+    "revision": "0e67b8ba46dc660e56e613d1b45bb132",
+    "url": "/static/media/tornado_damage_trees.0e67b8ba.jpg"
+  },
+  {
+    "revision": "ec557166ec70fede450aff6e0923ec26",
+    "url": "/static/media/plant.ec557166.jpg"
+  },
+  {
+    "revision": "bda14a168a2cba1cf7df1591bea2f58d",
+    "url": "/static/media/photosynthDiagram.bda14a16.jpg"
+  },
+  {
+    "revision": "1bd459eaa322a3550fc3912bd25648b5",
+    "url": "/static/media/hurricane_destruction.1bd459ea.jpg"
+  },
+  {
+    "revision": "112c5f7db34c34661b16a96ce21881cf",
+    "url": "/static/media/primary_succession_volcanic_rock.112c5f7d.jpg"
+  },
+  {
+    "revision": "5d86f0072fda3fe0bc121542e0249016",
+    "url": "/static/media/lichens.5d86f007.jpg"
+  },
+  {
+    "revision": "3a29627cf1eb70715be9f4704ec76b5e",
+    "url": "/static/media/primary_succession.3a29627c.png"
+  },
+  {
+    "revision": "de9bb743a9ef52f2ba21947f0ab6003c",
+    "url": "/static/media/tornado.de9bb743.gif"
+  },
+  {
+    "revision": "e15931338ef1772f96c7ad946adbce43",
+    "url": "/static/media/flooding.e1593133.gif"
+  },
+  {
+    "revision": "84405ee7d49a49489f331974f539d022",
+    "url": "/static/media/earthquake.84405ee7.gif"
+  },
+  {
+    "revision": "a07d0c47dcc752054044b4f113df555d",
+    "url": "/static/media/hurricane.a07d0c47.gif"
+  },
+  {
+    "revision": "b9b1fef4bc7310dea848b5b919f88ccf",
+    "url": "/static/media/wildflower.b9b1fef4.gif"
+  },
+  {
+    "revision": "0af474c7c2553948ff2e4288406e291d",
+    "url": "/static/media/vines_growing.0af474c7.gif"
+  },
+  {
+    "revision": "c151c9f8771a9b48766b54e8b008394f",
+    "url": "/static/media/sprouting_grasses.c151c9f8.gif"
+  },
+  {
+    "revision": "e4c77badc33d87342b2102336d4f75fa",
+    "url": "/static/media/lichens.e4c77bad.gif"
+  },
+  {
+    "revision": "88a6167dd765ccbc64f911228c3c2ac5",
+    "url": "/static/media/climax_community.88a6167d.gif"
+  },
+  {
+    "revision": "186405e38547c874ae357506887aeb9e",
+    "url": "/static/media/ecosystem.186405e3.jpg"
+  },
+  {
+    "revision": "d2b2a29d15c5928a2883023f93e73843",
+    "url": "/static/media/foodweb.d2b2a29d.jpg"
+  },
+  {
+    "revision": "24414f3ebdcc43518135501f72360e4e",
+    "url": "/index.html"
+  }
+];
