859e296e7981eb950dc92d22ee826eae046a6270
Logan Wood <lwood3499@gmail.com>
2019-04-08 21:49:28 -0500

    completed 7.12C content, working on 7.12F content

--- src/components/ContentPages/TEKSContent/TEKS7_12C/Notes7_12C.js
+++ src/components/ContentPages/TEKSContent/TEKS7_12C/Notes7_12C.js
@@ -8,6 +8,8 @@ import cls from '../../../Templates/NotesTemplate/NotesTemplate.module.css';
 //////////////////// PICTURE IMPORTS /////////////////////////////////
 import levelsOfOrg from './images/levels_of_organization.jpg';
 import leaf_anatomy from './images/leaf_anatomy.jpg';
+import apple from './images/apple.jpg';
+import muscle_tissue from './images/muscle_tissue.jpg';
 
 const notes7_12C = props => {
         return (
@@ -55,6 +57,42 @@ const notes7_12C = props => {
                                                 It's important to realize that not only animals are made up of
                                                 organs and organ systems, but also plants.
                                         </p>
+                                </div>
+                        </div>
+                        <div className={cls.NotesContainer}>
+                                <div className={cls.ImageContainer}>
+                                        <img className={cls.Image} src={apple} alt="apple" />
+                                </div>
+                                <div className={cls.wideParagraph}>
+                                        <p>
+                                                One example that always surprises students is understanding
+                                                that an apple is a reproductive organ. Complex organisms that
+                                                are made up of many cells are called <em>eukaryotes.</em>
+                                        </p>
+                                        <p>
+                                                There are simple organisms that do not contain structures
+                                                called organelles. They are called prokaryotes. An example of
+                                                an organelle is the nucleus in a eukaryote.
+                                        </p>
+                                </div>
+                        </div>
+                        <div className={cls.NotesContainer}>
+                                <div className={cls.Notes}>
+                                        <p>
+                                                Tissues are groups of cells that work together to perform a
+                                                task. There are many different types of cells in that have
+                                                many different shapes, but when they are grouped together,
+                                                like these muscle cells, then they form tissue.
+                                        </p>
+                                        <p>
+                                                Muscle cells are long and elastic, which makes sense because
+                                                muscle tissue needs to be elastic as its job is to contarct
+                                                and relax. It needs to be elastic enough to be pulled and
+                                                stretched, but also able to contract and do the pulling.
+                                        </p>
+                                </div>
+                                <div className={cls.ImageContainer}>
+                                        <img className={cls.Image} src={muscle_tissue} alt="muscle tissue" />
                                 </div>
                         </div>
                 </NotesTemplate>
--- src/components/ContentPages/TEKSContent/TEKS7_12F/Notes7_12F.js
+++ src/components/ContentPages/TEKSContent/TEKS7_12F/Notes7_12F.js
@@ -3,9 +3,11 @@ import React from 'react';
 import NotesTemplate from '../../../Templates/NotesTemplate/NotesTemplate';
 import Obj7_12F from './Obj7_12F';
 
-// import cls from '../../../Templates/NotesTemplate/NotesTemplate.module.css';
+import cls from '../../../Templates/NotesTemplate/NotesTemplate.module.css';
 
 //////////////////// PICTURE IMPORTS /////////////////////////////////
+import Robert_Hooke from './images/Robert_Hooke.jpg';
+import cells from './images/cells.jpg';
 
 const notes7_12F = props => {
         return (
@@ -15,7 +17,36 @@ const notes7_12F = props => {
                         teksQuiz={Obj7_12F.teksQuiz}
                         title={Obj7_12F.teksLabel}
                 >
-                        <h1>notes7_12F</h1>
+                        <div className={cls.NotesContainer}>
+                                <div className={cls.ImageContainer}>
+                                        <img className={cls.Image} src={Robert_Hooke} alt="Robert Hooke" />
+                                </div>
+                                <div className={cls.Notes}>
+                                        <p>
+                                                Robert Hooke first discovered what he later called "cells" in
+                                                1665. After he took a slice of cork and looked at it under a
+                                                microscope. The discovery eventually lead other scientists
+                                                along the way to make other contributions, such as Anton Van
+                                                Leewenhoek, Matthias Schleiden, Theodor Schwann, and Virchow.
+                                        </p>
+                                </div>
+                        </div>
+                        <div className={cls.NotesContainer}>
+                                <div className={cls.Notes}>
+                                        <p>
+                                                The <em>Cell Theory</em> was eventually developed, which
+                                                includes these 3 main components:
+                                        </p>
+                                        <ol>
+                                                <li>All living things are made up of one or more cells.</li>
+                                                <li>Cells are the basic unit of life.</li>
+                                                <li>Celles come from pre-existing cells.</li>
+                                        </ol>
+                                </div>
+                                <div className={cls.ImageContainer}>
+                                        <img className={cls.Image} src={cells} alt="cells" />
+                                </div>
+                        </div>
                 </NotesTemplate>
         );
 };
