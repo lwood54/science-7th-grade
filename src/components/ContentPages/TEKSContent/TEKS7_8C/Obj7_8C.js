9ce57ab5132c054d26d8ea8add11ba9f1f228708
Logan Wood <lwood3499@gmail.com>
2019-04-16 21:05:23 -0500

    added content to 6.1

--- src/components/ContentPages/TEKSContent/TEKS7_8C/Obj7_8C.js
+++ src/components/ContentPages/TEKSContent/TEKS7_8C/Obj7_8C.js
@@ -3,7 +3,42 @@ const obj7_8C = {
         teksLabel: 'Groundwater and Surface Water',
         teksContent: 'Model the effects of human activity on groundwater and surface water in a watershed.',
         teksQuiz: '/quiz7_8C',
-        questions: []
+        questions: [
+                {
+                        text: '__________ is the land area that drains to a common river or stream.',
+                        answers: [
+                                { wrong1: 'water table' },
+                                { wrong2: 'aquifer' },
+                                { wrong3: 'groundwater' },
+                                { correct: 'watershed' }
+                        ],
+                        image: ''
+                },
+                {
+                        text: 'What is the best description of groundwater.',
+                        answers: [
+                                {
+                                        wrong1:
+                                                'It is water that is found ontop of the ground, like lakes, rivers, and streams.'
+                                },
+                                { wrong2: 'Water that forms from plants on the ground.' },
+                                { wrong3: 'It is water that is has been mixed up into many pieces.' },
+                                { correct: 'It is the water that has soaked into the ground.' }
+                        ],
+                        image: ''
+                },
+                {
+                        text:
+                                'Which of the following is a problem that increased pavement and concrete in an ecosystem will result in?',
+                        answers: [
+                                { wrong1: 'An increase in local vegetation.' },
+                                { wrong2: 'A decrease in pollution.' },
+                                { wrong3: 'An increase in the rate of precipitation.' },
+                                { correct: 'An increase in runoff and erosion.' }
+                        ],
+                        image: require('./images/runoff.JPG')
+                }
+        ]
 };
 
 export default obj7_8C;
